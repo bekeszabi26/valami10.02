702464ce6d6649b00be01c73e25460aa7c14f32d
b19BekeS <b19BekeS@204-8>
2023-10-02 12:49:57 +0200

    Add project files.

---
+++ valami10.02/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace valami10._02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Szabi";
+            Console.WriteLine($"Hello, {name}");
+        }
+    }
+}
